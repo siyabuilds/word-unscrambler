0621f931511f1e3e49e05388e3ee178b2b211987
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2025-02-19 15:31:12 +0200

    Add more colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,12 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        lightGray: "#f7f7f7",
+        darkGray: "#333333",
+        black: "#000000",
+        white: "#FFFFFF",
+        lightBlue: "#4f83cc",
+        red: "#ff4d4d",
       },
     },
   },
